9d2f26d57556e62e7387b59c9f3976673665a626
aizhuldyz gabbassova <aigabbass90@gmail.com>
2018-03-13 19:20:06 +0600

    synchronous solution

---
+++ MultiThreading/ClientSync/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        private static Thread writeThread;
+        private static Thread readThread;
+        private static bool Stopped;
+        private static Socket serviceSocket = new Socket
+            (AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        private static Socket messageSocket = new Socket
+            (AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        private static readonly List<string> MessageList = new List<string>
+        {
+            "hello!", "I think i know the answer!",
+            "no kidding"
+        };
+        static void Main(string[] args)
+        {
+            Console.Title = "Client";
+            serviceSocket.Connect(IPAddress.Loopback, 5555);
+            messageSocket.Connect(IPAddress.Loopback, 5556);
+            Write(serviceSocket, "login");
+            var s = Read(serviceSocket);
+            if (s != "OK")
+            {
+                Console.WriteLine("Cannot login");
+                Console.ReadLine();
+                return;
+            }
+            writeThread = new Thread(WriteThread);
+            writeThread.Start();
+            readThread = new Thread(ReadThread);
+            readThread.Start();
+            Console.ReadLine();
+            Stopped = true;
+            readThread.Abort();
+            writeThread.Abort();
+        }
+
+        private static void WriteThread()
+        {
+            while (!Stopped)
+            {
+                var index = new Random().Next(0, MessageList.Count -1);
+                var message = MessageList.ElementAt(index);
+                Write(messageSocket, message);
+                Thread.Sleep(5000);
+            }
+        }
+
+        private static void ReadThread()
+        {
+            while (!Stopped)
+            {
+                var s = Read(messageSocket);
+                Console.WriteLine("Message: " + s);
+            }
+        }
+
+        private static void Write(Socket socket, string text)
+        {
+            byte[] buffer = Encoding.UTF8.GetBytes(text);
+            socket.Send(buffer, 0, buffer.Length, SocketFlags.None);
+        }
+
+        private static string Read(Socket socket)
+        {
+            byte[] buffer = new byte[10240];
+            int received = socket.Receive(buffer, SocketFlags.None);
+            byte[] recBuf = new byte[received];
+            Array.Copy(buffer, recBuf, received);
+            return Encoding.UTF8.GetString(recBuf);
+        }
+    }
+}
---
+++ MultiThreading/ServerSync/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        private static readonly Socket serviceSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        private static readonly Socket messageSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        private static readonly List<Socket> clientSockets = new List<Socket>();
+        private const int BUFFER_SIZE = 2048;
+        private const int readPort = 5555;
+        private const int writePort = 5556;
+        private static readonly byte[] buffer = new byte[BUFFER_SIZE];
+        private static readonly int messageCount = 10;
+        private static bool Stopped;
+        private static Thread serviceThread;
+        private static Thread messageThread;
+
+        static void Main(string[] args)
+        {
+            Console.Title = "Server";
+            serviceSocket.Bind(new IPEndPoint(IPAddress.Any, readPort));
+            serviceSocket.Listen(10);
+            messageSocket.Bind(new IPEndPoint(IPAddress.Any, writePort));
+            messageSocket.Listen(10);
+            serviceThread = new Thread(ServiceThread);
+            serviceThread.Start();
+            messageThread = new Thread(MessageAcceptThread);
+            messageThread.Start();
+            Console.ReadLine();
+            Stopped = true;
+            messageThread.Abort();
+            serviceThread.Abort();
+        }
+
+        private static void ServiceThread()
+        {
+            while (!Stopped)
+            {
+                var socket = serviceSocket.Accept();
+                var login = Read(socket);
+                if (login == "login")
+                {
+                    Write(socket, "OK");
+
+                }
+                else
+                {
+                    Write(socket, "Access denied");
+                }
+            }
+        }
+
+        private static void MessageAcceptThread()
+        {
+            while (!Stopped)
+            {
+                var socket = messageSocket.Accept();
+                clientSockets.Add(socket);
+                new Thread(MessageThread).Start(socket);
+            }
+            
+        }
+
+        private static void MessageThread(Object obj)
+        {
+            var socket = (Socket) obj;
+            while (!Stopped)
+            {
+                var message = Read(socket);
+                foreach (var client in clientSockets)
+                {
+                    if (client != socket)
+                    {
+                        Write(client, message);
+                    }
+
+                }
+            }
+        }
+
+        private static void Write(Socket socket, string text)
+        {
+            byte[] buffer = Encoding.UTF8.GetBytes(text);
+            socket.Send(buffer, 0, buffer.Length, SocketFlags.None);
+        }
+
+        private static string Read(Socket socket)
+        {
+            byte[] buffer = new byte[10240];
+            int received = socket.Receive(buffer, SocketFlags.None);
+            byte[] recBuf = new byte[received];
+            Array.Copy(buffer, recBuf, received);
+            return Encoding.UTF8.GetString(recBuf);
+        }
+    }
+}
