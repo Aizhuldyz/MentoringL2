a8537378b1062407d7e8922be1dd779d7c2d5115
Aizhuldyz <aigabbass90@gmail.com>
2018-03-21 21:28:40 +0600

    File formating fix

--- ExpressionsAndQueryable/HomeTask2/ExpressionToFTSRequestTranslator.cs
+++ ExpressionsAndQueryable/HomeTask2/ExpressionToFTSRequestTranslator.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
@@ -7,79 +7,82 @@ using System.Threading.Tasks;
 
 namespace HomeTask2
 {
-	public class ExpressionToFtsRequestTranslator : ExpressionVisitor
-	{
-		private StringBuilder _resultString;
-	    private IList<string> _queries;
+    public class ExpressionToFtsRequestTranslator : ExpressionVisitor
+    {
+        private StringBuilder _resultString;
+        private IList<string> _queries;
 
-	    public ExpressionToFtsRequestTranslator(IList<string> queries)
-	    {
-	        _queries = queries;	        
+        public ExpressionToFtsRequestTranslator(IList<string> queries)
+        {
+            _queries = queries;
         }
 
-	    public IList<string> TranslateQuery(Expression exp)
-	    {
-	        Translate(exp);
-	        return _queries;
-	    }
+        public IList<string> TranslateQuery(Expression exp)
+        {
+            Translate(exp);
+            return _queries;
+        }
 
-	    public void Translate(Expression exp)
-		{
-		    _resultString = new StringBuilder();
+        public void Translate(Expression exp)
+        {
+            _resultString = new StringBuilder();
             Visit(exp);
-		    if (_resultString.Length != 0)
-		    {
-		        _queries.Add(_resultString.ToString());
-		    }
-		}
+            if (_resultString.Length != 0)
+            {
+                _queries.Add(_resultString.ToString());
+            }
+        }
 
-		protected override Expression VisitMethodCall(MethodCallExpression node)
-		{
-			if (node.Method.DeclaringType == typeof(Queryable)
-				&& node.Method.Name == "Where")
-			{
-				var predicate = node.Arguments[1];
-				Visit(predicate);
+        protected override Expression VisitMethodCall(MethodCallExpression node)
+        {
+            if (node.Method.DeclaringType == typeof(Queryable)
+                && node.Method.Name == "Where")
+            {
+                var predicate = node.Arguments[1];
+                Visit(predicate);
 
-				return node;
-			}
+                return node;
+            }
 
-            if (node.Method.DeclaringType == typeof(string)) {
+            if (node.Method.DeclaringType == typeof(string))
+            {
                 var predicate = node.Arguments[0];
                 string newConstantValue = "";
                 switch (node.Method.Name)
                 {
                     case "StartsWith":
-                            newConstantValue = ((ConstantExpression)predicate).Value + "*";
-                            break;
+                        newConstantValue = ((ConstantExpression)predicate).Value + "*";
+                        break;
                     case "EndsWith":
-                            newConstantValue = "*" + ((ConstantExpression)predicate).Value;
-                            break;
+                        newConstantValue = "*" + ((ConstantExpression)predicate).Value;
+                        break;
                     case "Contains":
-                            newConstantValue = "*" + ((ConstantExpression)predicate).Value + "*";
-                            break;
+                        newConstantValue = "*" + ((ConstantExpression)predicate).Value + "*";
+                        break;
                 }
-                if (newConstantValue != "") {
+                if (newConstantValue != "")
+                {
                     BuildQueryString(node.Object, Expression.Constant(newConstantValue));
-                    return node; 
+                    return node;
                 }
 
             }
 
             return base.VisitMethodCall(node);
-		}
+        }
 
-		protected override Expression VisitBinary(BinaryExpression node)
-		{
-			switch (node.NodeType)
-			{
-				case ExpressionType.Equal:
-                    if (node.Left.NodeType == ExpressionType.MemberAccess && node.Right.NodeType == ExpressionType.Constant)                        
+        protected override Expression VisitBinary(BinaryExpression node)
+        {
+            switch (node.NodeType)
+            {
+                case ExpressionType.Equal:
+                    if (node.Left.NodeType == ExpressionType.MemberAccess && node.Right.NodeType == ExpressionType.Constant)
                     {
                         BuildQueryString(node.Left, node.Right);
                         break;
                     }
-                    if (node.Left.NodeType == ExpressionType.Constant && node.Right.NodeType == ExpressionType.MemberAccess) {
+                    if (node.Left.NodeType == ExpressionType.Constant && node.Right.NodeType == ExpressionType.MemberAccess)
+                    {
                         BuildQueryString(node.Right, node.Left);
                         break;
                     }
@@ -89,25 +92,25 @@ namespace HomeTask2
                     new ExpressionToFtsRequestTranslator(_queries).Translate(node.Right);
                     break;
                 default:
-					throw new NotSupportedException($"Operation {node.NodeType} is not supported");
-			};
+                    throw new NotSupportedException($"Operation {node.NodeType} is not supported");
+            };
 
-			return node;
-		}
+            return node;
+        }
 
-		protected override Expression VisitMember(MemberExpression node)
-		{
-			_resultString.Append(node.Member.Name).Append(":");
+        protected override Expression VisitMember(MemberExpression node)
+        {
+            _resultString.Append(node.Member.Name).Append(":");
 
-			return base.VisitMember(node);
-		}
+            return base.VisitMember(node);
+        }
 
-		protected override Expression VisitConstant(ConstantExpression node)
-		{
-			_resultString.Append(node.Value);
+        protected override Expression VisitConstant(ConstantExpression node)
+        {
+            _resultString.Append(node.Value);
 
-			return node;
-		}
+            return node;
+        }
 
         private void BuildQueryString(Expression member, Expression constant)
         {
